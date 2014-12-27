c0c5189e84d9f25fdc6ed955572302f1fbc4006d
Jesse van der Sar <jesse@sar.nl>
2014-12-27 17:34:57 +0100

    Portal is now automatically created with a new client

--- clients/models.py
+++ clients/models.py
@@ -11,6 +11,14 @@ class Client(models.Model):
 	company = models.CharField(max_length = 255)
 	name = models.CharField(max_length = 255)
 		
+	def save(self, *args, **kwargs):
+		is_new = self.pk is None
+		super(Client, self).save(*args, **kwargs)
+		
+		# Automatically create client portal if client is new
+		if is_new:
+		    self.portal = Portal(client=self)
+		    self.portal.save()
 	
 class Portal(models.Model):
 	class Meta:
