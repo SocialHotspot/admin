d84ca0c5fa4528fe279f979da71f8fb5d3709e22
Jesse van der Sar <jesse@sar.nl>
2014-12-27 17:41:45 +0100

    Made auto-creating portals a lot cleaner

--- clients/models.py
+++ clients/models.py
@@ -1,27 +1,25 @@
 from django.db import models
+from django.db.models.signals import post_save
 
-class BaseModel(models.Model):
-	class Meta:
-		abstract = True
-		
+# Clients		
 class Client(models.Model):
 	class Meta:
 		db_table = 'client'
 	
 	company = models.CharField(max_length = 255)
 	name = models.CharField(max_length = 255)
-		
-	def save(self, *args, **kwargs):
-		is_new = self.pk is None
-		super(Client, self).save(*args, **kwargs)
-		
-		# Automatically create client portal if client is new
-		if is_new:
-		    self.portal = Portal(client=self)
-		    self.portal.save()
-	
+
+# Portals	
 class Portal(models.Model):
 	class Meta:
 		db_table = 'portal'
 		
-	client = models.OneToOneField(Client, related_name='portal')
+	client = models.OneToOneField(Client, related_name = 'portal')
+	
+# Automatically create client portal if client is new
+def create_client_portal(sender, instance, created, **kwargs):
+	if created:
+	    portal = Portal(client = instance)
+	    portal.save()
+	
+post_save.connect(create_client_portal, sender = Client)
