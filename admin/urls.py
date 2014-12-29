2420a12f8b230ef272edcc30bf661980294cde68
Jesse van der Sar <jesse@sar.nl>
2014-12-29 04:11:28 +0100

    Added clear headers to clients overview

--- admin/urls.py
+++ admin/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url='/clients/')),
+
 	url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^clients/', include('clients.urls', namespace='clients')),
 )
--- clients/urls.py
+++ clients/urls.py
@@ -5,6 +5,7 @@ from clients.views import ClientList, ClientDetail, add
 
 urlpatterns = patterns('',
     url(r'^$', login_required(ClientList.as_view()), name='overview'),
+    
     url(r'^detail/(?P<slug>[\w-]+)/$', login_required(ClientDetail.as_view()), name='detail'),
     url(r'^add/$', add, name='add'),
 )
